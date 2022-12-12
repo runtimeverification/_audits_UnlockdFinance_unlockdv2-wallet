c54ac4cbb83173289cbbabdb3784395b354be298
Lisandro Corbalan <lmcorbalan@gmail.com>
2022-12-12 15:06:24 -0300

    Initialize repo

---
+++ README.md
@@ -0,0 +1,41 @@
+# Rights Delegation Wallet
+
+## Installation
+
+_Having issues? See the [troubleshooting section](https://github.com/foundry-rs/foundry/blob/master/README.md#troubleshooting-installation)_.
+
+First run the command below to get `foundryup`, the Foundry toolchain installer:
+
+```sh
+curl -L https://foundry.paradigm.xyz | bash
+```
+
+If you do not want to use the redirect, feel free to manually download the
+foundryup installation script from
+[here](https://raw.githubusercontent.com/foundry-rs/foundry/master/foundryup/foundryup).
+
+Then, run `foundryup` in a new terminal session or after reloading your `PATH`.
+
+Other ways to use `foundryup`, and other documentation, can be found [here](https://github.com/foundry-rs/foundry/tree/master/foundryup). Happy forging!
+
+## Install dependencies
+
+### Yarn
+
+```
+yarn install
+```
+
+### Forge
+
+```
+forge install
+```
+
+## Run tests
+
+Create an `.env` file using `.env.example` as template, then run
+
+```
+yarn test:all
+```
---
+++ hardhat.config.ts
@@ -0,0 +1,54 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-deploy";
+
+dotenv.config();
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+let accounts: any[] = [];
+if (process.env.PRIVATE_KEY !== undefined) {
+  accounts = [process.env.PRIVATE_KEY];
+} else {
+  throw new Error(`PRIVATE_KEY not set`);
+}
+
+const config: HardhatUserConfig = {
+  // solidity: "0.8.13",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  paths: {
+    artifacts: "./artifacts",
+    sources: "./src",
+  },
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_RPC || "",
+      accounts: accounts,
+    },
+  },
+  namedAccounts: {
+    deployer: { default: 0 },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.YOUR_ETHERSCAN_API_KEY,
+  },
+};
+
+export default config;
